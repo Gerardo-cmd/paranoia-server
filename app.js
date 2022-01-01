2e1fe27d02cf2a3022ddc36ca439c993826e4e37
Gerardo Abaunza <56178845+Gerardo-cmd@users.noreply.github.com>
2021-12-31 19:14:09 -0500

    Added users endpoint

--- app.js
+++ app.js
@@ -36,8 +36,7 @@ const db = admin.firestore();
 // Endpoints start below
 
 app.get('/', (req, res) => {
-  res.send({
-    "code": 200,
+  res.status(200).send({
     "msg": "Connected"
   });
 });
@@ -47,8 +46,7 @@ app.get('/', (req, res) => {
 // Create a new room and returns the room code
 app.post('/new-room', async (req, res) => {
   if (!req.body.userId) {
-    res.send({
-      "code": 400,
+    res.status(400).send({
       "data": "Must include all necessary info!"
     });
     return;
@@ -69,11 +67,12 @@ app.post('/new-room', async (req, res) => {
     answers: {}
   }
 
-  const setUser = await usersRef.set({creator: true});
-  const setBody =  await roomRef.set(newRoom);
-  res.send({
-    "code": 200,
-    "data": roomCode
+  await usersRef.set({name: req.body.userId, creator: true});
+  await roomRef.set(newRoom);
+  res.status(200).send({
+    "data": {
+      roomCode
+    }
   });
   return;
 });
@@ -81,31 +80,27 @@ app.post('/new-room', async (req, res) => {
 // Adds a user to an existing room and returns room code
 app.post('/new-player', async (req, res) => {
   if (!req.body.userId || !req.body.roomCode ) {
-    res.send({
-      "code": 400,
+    res.status(400).send({
       "data": "Must include both user ID and room code!"
     });
     return;
   }
   const roomRef = await db.collection('rooms').doc(`${req.body.roomCode}`).get();
   if (!roomRef.exists) {
-    res.send({
-      "code": 401,
+    res.status(401).send({
       "msg": "No room was found with that code"
     });
     return;
   }
   const userRefTest = await db.collection(`${req.body.roomCode}-Users`).doc(`${req.body.userId}`).get();
   if (userRefTest.exists) {
-    res.send({
-      "code": 402,
+    res.status(402).send({
       "msg": "There is already a user with that name in the room"
     });
     return;
   }
-  const userRef = await db.collection(`${req.body.roomCode}-Users`).doc(`${req.body.userId}`).set({creator: false});
-  res.send({
-    "code": 200,
+  const userRef = await db.collection(`${req.body.roomCode}-Users`).doc(`${req.body.userId}`).set({name: req.body.userId, creator: false});
+  res.status(200).send({
     "data": {
       roomCode: req.body.roomCode,
       name: req.body.userId
@@ -114,19 +109,45 @@ app.post('/new-player', async (req, res) => {
   return;
 });
 
+// Returns all of the room's users and their info
+app.get('/users/:roomId', async (req, res) => {
+  console.log(req.params.roomId);
+  const roomRef = await db.collection('rooms').doc(`${req.params.roomId}`).get();
+  if (!roomRef.exists) {
+    res.status(401).send({
+      "msg": "No room was found with that code"
+    });
+    return;
+  }
+  let users = [];
+  const usersRefTest = (await db.collection(`${req.params.roomId}-Users`).get()).docs;
+  console.log(usersRefTest);
+  usersRefTest.forEach((doc) => {
+    console.log(doc._fieldsProto);
+    users.push({
+      name: doc._fieldsProto.name.stringValue,
+      creator: doc._fieldsProto.creator.booleanValue
+    });
+  })
+  res.status(200).send({
+    "data": {
+      users
+    }
+  });
+  return;
+});
+
 // Deletes a room
 app.delete('/room', async (req, res) => {
   if (!req.body.roomCode) {
-    res.send({
-      "code": 400,
+    res.status(400).send({
       "msg": "Must include the room code"
     });
     return;
   }
   const docRef = await db.collection('rooms').doc(`${req.body.roomCode}`).get();
   if (!docRef.exists) {
-    res.send({
-      "code": 401,
+    res.status(401).send({
       "msg": "No room was found with that code"
     });
     return;
@@ -139,8 +160,7 @@ app.delete('/room', async (req, res) => {
   snapshot.forEach(doc => {
     doc._ref.delete();
   });
-  res.send({
-    "code": 200,
+  res.status(200).send({
     "msg": "Room deleted"
   })
 });
@@ -149,17 +169,15 @@ app.delete('/room', async (req, res) => {
 
 // Adds a quesion to an existing room
 app.post('/new-question', async (req, res) => {
-  if (!req.body.askerId || !req.body.roomCode  || !req.body.victimId || !req.body.message || ((req.body.shown === null) || (req.body.shown === undefined))) {
-    res.send({
-      "code": 400,
+  if (!req.body.askerId || !req.body.roomCode  || !req.body.victimId || !req.body.message || !req.body.answer || ((req.body.shown === null) || (req.body.shown === undefined))) {
+    res.status(400).send({
       "data": "Must include all info"
     });
     return;
   }
   const roomRef = await db.collection('rooms').doc(`${req.body.roomCode}`).get();
   if (!roomRef.exists) {
-    res.send({
-      "code": 401,
+    res.status(401).send({
       "msg": "No room was found with that code"
     });
     return;
@@ -174,7 +192,6 @@ app.post('/new-question', async (req, res) => {
   // If there are already questions in the room, copy them into the "newQuestions" object
   for (const [key, value] of Object.entries(questions)) {
     let question = {};
-    console.log("Now entering the fields of the question");
     for (const [field, value1] of Object.entries(value.mapValue.fields)) {
       if (field === "shown") {
         question[field] = value1.booleanValue;
@@ -192,6 +209,7 @@ app.post('/new-question', async (req, res) => {
     askerId: req.body.askerId,
     victimId: req.body.victimId,
     message: req.body.message,
+    answer: req.body.answer,
     shown: req.body.shown
   }
   const ref = db.collection('rooms').doc(`${req.body.roomCode}`);
@@ -200,8 +218,7 @@ app.post('/new-question', async (req, res) => {
   await ref.update({
     questions: newQuestions
   });
-  res.send({
-    "code": 200,
+  res.status(200).send({
     "data": {
       "questionKey": length,
     }
